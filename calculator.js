f53ee98be61eaa767e5e3ceaeb4fb2d6897fef83
Alexis Ocampo <al3xisoc4mpo@gmail.com>
2022-01-11 11:15:35 -0600

    Validaciones realizadas

--- calculator.js
+++ calculator.js
@@ -1,12 +1,57 @@
+console.log ("Hola Mundo!");
+
+// FUNCIONES DECLARATIVAS
+// SOLO SE HACE UNA VEZ
+// function
 function sum(a, b) {
-  return;
+
+  // EN CASO DE QUE NO HAYA ARGUMENTOS
+  if (!a && !b) {
+    return 0;
+  }
+
+  // EN CASO QUE EL SEGUNDO ARGUMENTO NO EXISTA
+  if (!b) {
+    b = 0;
+  }
+
+  const total = a + b;
+  return total;
 }
 
+// INVOCACION DE FUNCION
+// PUEDE USARSE (INVOCARSE) LAS VECES QUE QUIERAN
+// sum(1,2)
+
 function subtract(a, b) {
-  return;
+
+    // EN CASO DE QUE NO HAYA ARGUMENTOS
+    if (!a && !b) {
+      return 0;
+    }
+  
+    // EN CASO QUE EL SEGUNDO ARGUMENTO NO EXISTA
+    if (!b) {
+      b = 0;
+    }
+  const total = a - b;
+  return total;
 }
 
 function divide(a, b) {
+  
+      // EN CASO DE QUE NO HAYA ARGUMENTOS
+      if (!a && !b) {
+        return 0;
+      }
+    
+      // EN CASO QUE EL SEGUNDO ARGUMENTO SEA CERO (LA DIVISION ENTRE CERO NO EXISTE)
+      if (b === 0) {
+       return error('Division by 0 not valid');
+      }
+
+    const total = a / b;
+    return total;
   return;
 }
 
